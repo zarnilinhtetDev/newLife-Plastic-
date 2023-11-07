e561b06ee353834cb422747ef22d5f804b32415e
win min <winminwork@gmail.com>
2023-11-07 09:41:14 +0800

    KoHtway

--- resources/views/blade/inout/inout.blade.php
+++ resources/views/blade/inout/inout.blade.php
@@ -54,13 +54,13 @@
                                 <div class="container-fluid">
                                     <div class="row mb-2">
                                         <div class="col-sm-6">
-                                            <h1>Expenses</h1>
+                                            <h1>ပေးရန်/ရရန်</h1>
                                         </div>
                                         <div class="col-sm-6">
                                             <ol class="breadcrumb float-sm-right">
                                                 <li class="breadcrumb-item"><a
                                                         href="{{ url('/dashboard') }}">Dashboard</a></li>
-                                                <li class="breadcrumb-item active">Expenses</li>
+                                                <li class="breadcrumb-item active">ပေးရန်/ရရန်</li>
                                             </ol>
                                         </div>
                                     </div>
